153b68524211a9e47e4b8e5d341a0c4a32370722
aibryx <alimdzhanibekov1@gmail.com>
2024-04-10 13:59:46 +0300

    fix import

--- src/modules/misc/pages/landing.tsx
+++ src/modules/misc/pages/landing.tsx
@@ -1,6 +1,6 @@
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
+// import { Label } from '@/components/ui/label';
 import { Search } from 'lucide-react';
 
 export const Landing = () => {
